b3662d1f96db2f9122dd27ccd97d87515adde8cf
Derick Carmelo <d.carmelo0248@gmail.com>
2026-09-09 22:22:02 -0300

    feat: add Node.js Supabase client

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+	process.env.SUPABASE_URL,
+	process.env.SUPABASE_KEY
+)
+
+const { data, error } = await supabase
+	.from('produtos')
+	.select('*')
+
+if (error) {
+	console.error('Erro:', error)
+} else {
+	console.log('Produtos:', data)
+}
