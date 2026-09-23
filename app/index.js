903a4e82787d6ff725c425928308551afd7462fd
Derick Carmelo <d.carmelo0248@gmail.com>
2026-09-23 20:25:29 -0300

    feat: add PostgreSQL service and direct database connection

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 import 'dotenv/config'
 import express from 'express'
 import { createClient } from '@supabase/supabase-js'
+import pg from 'pg'
+
+const { Pool } = pg
 
 const app = express()
 
@@ -8,6 +11,14 @@ const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_KEY
 )
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
 
 app.get('/produtos', async (req, res) => {
     const { data, error } = await supabase
@@ -38,4 +49,18 @@ app.listen(3000, '0.0.0.0', () => {
     console.log('API rodando na porta 3000')
 })
 
+app.get('/produtos-local', async (req, res) => {
+    try {
+        const resultado = await pool.query(
+            'SELECT * FROM produtos ORDER BY id'
+        )
 
+        res.json(resultado.rows)
+    } catch (error) {
+	  console.error('ERRO POSTGRES:', error)
+
+        res.status(500).json({
+            error: error.message
+        })
+    }
+})
